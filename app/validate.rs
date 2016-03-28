24ef8ddd46c135659c0885ceab8328b03237fb87
ddabek <xordaniel@gmail.com>
2016-03-28 12:09:20 +0100

    extern crate missing

--- app/validate.rs
+++ app/validate.rs
@@ -2,6 +2,7 @@
 //!
 //! This application returns the results of a Poll and validates all participating transactions
 //!
+extern crate vote;
 
 use vote::voting::validate_genesis::{VotingOutcome};
 
